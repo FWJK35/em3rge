97376b9b2e1f8aa7ae37f24562458c73f6f9bd30
FWJK35 <calvinrodrigue@gmail.com>
2023-05-31 15:09:31 -0700

    added rules display, physics updates
    Co-authored-by: es <apidges@users.noreply.github.com>

--- RealPhysics.java
+++ RealPhysics.java
@@ -2,8 +2,12 @@ public class RealPhysics extends Physics {
     private double[] mass;
 
     // constructor
-    public RealPhysics(int types) {
+    public RealPhysics() {
         super();
+    }
+
+    public RealPhysics(int types) {
+        super(types);
 
         mass = new double[types];
         for (int i = 0; i < types; i++) {
@@ -28,15 +32,32 @@ public class RealPhysics extends Physics {
         if (realDist > Particle.RADIUS * 2) {
             return false;
         }
+        
+        // adjusting positions outside each other
+        for (int i = 0; i < World.dimensions; i++) {
+            double dist = .5 * a.distance(i, b);
+            dist *= -1;
+            if (dist < 0) {
+                dist += Particle.RADIUS;
+            }
+            else {
+                dist -= Particle.RADIUS;
+            }
+            a.addPosition(i, dist);
+            b.addPosition(i, -dist);
+        }
+        // TODO: post-collision velocity changes
         return true;
     }
 
     @Override
     public double getForce(Particle a, Particle b, double realDist) {
-        if (collision(a, b, realDist)) {
+        if (collision(a, b, realDist) || getUpdateDistance() < realDist) {
             return 0;
         }
         
-        return 0;
+        double force = 1/realDist;
+        force *= getForceScale() * mass[a.getType()] * mass[b.getType()] * force;
+        return force;
     }
 }
